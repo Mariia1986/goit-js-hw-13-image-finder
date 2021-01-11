87e6c857a4587e903e22c4ec16e53c8adffbc525
Mariia Randarevych <renij1986@gmail.com>
2021-01-11 13:19:21 +0300

    add scroll

--- src/index.js
+++ src/index.js
@@ -4,46 +4,35 @@ import refs from './js/refs';
 const { form, searchBtn, input, gallery, loadmoreBtn } = refs;
 import search from './js/apiService.js';
 
-
 form.addEventListener('submit', renderImg);
-// form.addEventListener('click', refreshlists);
-loadmoreBtn.addEventListener('click',newpage
-);
+
+loadmoreBtn.addEventListener('click', newpage);
 
 function renderImg(e) {
   e.preventDefault();
-  gallery.innerHTML='';
+  gallery.innerHTML = '';
   search.resetPage();
   search.query = e.target.elements.query.value;
   if (search.query === '') {
     return;
-
   }
-  
-  search.searchImg().then(renderImglist)
-}
- function renderImglist(arr) {
-  
 
+  search.searchImg().then(renderImglist);
+}
+function renderImglist(arr) {
   const markup = imgitem(arr);
   gallery.insertAdjacentHTML('beforeEnd', markup);
   loadmoreBtn.classList.remove('is-hidden');
- 
-  
-    window.scrollBy({
-     
-      top: window.innerHeight - 10,
-      behavior: 'smooth',
-    });
-    input.value=''
 
+  window.scrollBy({
+    top: gallery.scrollHeight,
+    behavior: 'smooth',
+  });
+  input.value = '';
 }
-// function refreshlists(e) {
-//   e.target.elements.query.value = '';
-// }
+
 function newpage() {
- 
- search.setPage()
- 
-  search.searchImg().then(renderImglist)
+  search.setPage();
+
+  search.searchImg().then(renderImglist);
 }
